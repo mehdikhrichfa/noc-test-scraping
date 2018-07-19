6e573c61130fe1cfb6a72be282cd8c82f94f9a42
mehdikhrichfa <khrichfa97@hotmail.fr>
2018-07-19 14:20:05 -0400

    Add code to create json file that stores pdf filenames and urls

--- main/pipelines.py
+++ main/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.pipelines.files import FilesPipeline
 import scrapy
+import json
 import main.settings
 import os
 
@@ -30,6 +31,8 @@ class PDFPipeline(FilesPipeline):
     #     print("\n\n\n\n\n\n\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\n\n\n\n\n\n")
     #     self.basepath = basepath
 
+    path_urls = dict()
+
     def file_path(self, request, response=None, info=None):
         # print(request.url)
         # print(self.basepath)
@@ -38,14 +41,18 @@ class PDFPipeline(FilesPipeline):
         if not question_mark == -1:
             # print("Question mark!!!")
             filename = filename[:question_mark]
-        # with open("full/" + os.path.splitext(filename)[0] + ".txt") as url_file:
-        #     url_file.write(request.url)
-        #
 
-        return "pdf/{}".format(str(filename))
+        self.path_urls[filename] = request.url
+
+        return str(filename)
 
     def get_media_requests(self, item, info):
         yield scrapy.Request(item['file_urls'][0])
+
+    def close_spider(self, spider):
+        # Write paths and urls
+        with open("path_urls.json", 'w', encoding='utf-8') as file:
+            file.write(json.dumps(self.path_urls))
 
     # def item_completed(self, results, item, info):
     #     if not results[0][0]:
