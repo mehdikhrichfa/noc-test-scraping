7139faaa5c514a0ec01bb203e50a9812f41f2c17
mehdikhrichfa <khrichfa97@hotmail.fr>
2018-08-07 11:05:20 -0400

    Document pipelines.py

--- main/pipelines.py
+++ main/pipelines.py
@@ -9,47 +9,49 @@ import scrapy
 import json
 
 
-class Pipeline(object):
-
-    def process_item(self, item, spider):
-        return item
-
-
 class PDFPipeline(FilesPipeline):
-    # count_404 = 0
-    # broken = dict()
-
-    # @classmethod
-    # def from_crawler(cls, crawler):
-    #     settings = crawler.settings
-    #     print("\n\nHERE\n\n")
-    #     basepath = settings.get('FILES_STORE')
-    #     return cls(basepath=basepath)
-    #
-    # def __init__(self, basepath):
-    #     print("\n\n\n\n\n\n\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\n\n\n\n\n\n")
-    #     self.basepath = basepath
+    """
+    Pipeline that processes pdf files download requests.
+    """
 
     path_urls = dict()
     url_names = dict()
 
     def file_path(self, request, response=None, info=None):
-        # print(request.url)
-        # print(self.basepath)
+        """
+        Changes the filename of the pdf file to download to its default name (from the sha name given by Scrapy)
+        :param request:
+        :param response:
+        :param info:
+        :return: String object with the new filename
+        """
         filename = request.url.split('/')[-1].replace('%20', ' ')
         question_mark = filename.rfind('?')
         if not question_mark == -1:
-            # print("Question mark!!!")
             filename = filename[:question_mark]
 
         self.path_urls[filename] = request.url
         return str(filename)
 
     def get_media_requests(self, item, info):
+        """
+        Associates, in a dictionary, URLs and titles of the files
+        :param item: Item object containing the URL the file will be downloaded from and the title of the file
+        :param info:
+        :return: Request object containing the file URL
+        """
+
         self.url_names[item['file_urls'][0]] = item['title'][:item['title'].rfind('|')].strip()
         yield scrapy.Request(item['file_urls'][0])
 
     def close_spider(self, spider):
+        """
+        Called when the spider is closed.
+        Writes 2 JSON files:
+            -   The first one associates file paths and file urls
+            -   The second one associates file urls and file titles
+        :param spider: Spider object that refers to the spider that was just closed
+        """
         # Write paths and urls
         with open('path_urls.json', 'w', encoding='utf-8') as file:
             file.write(json.dumps(self.path_urls, indent=4))
@@ -57,10 +59,3 @@ class PDFPipeline(FilesPipeline):
         # Write names and urls
         with open('url_names.json', 'w', encoding='utf-8') as file:
             file.write(json.dumps(self.url_names, indent=4))
-
-        # with open('broken.json', 'w', encoding='utf-8') as broken_pdfs:
-        #     broken_pdfs.write(json.dumps(self.broken, indent=4))
-
-    # def item_completed(self, results, item, info):
-    #     if not results[0][0]:
-    #         self.broken[item['url']] = item['file_urls'][0]
